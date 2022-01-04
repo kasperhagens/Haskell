197a4ebc40259a0e4e9215e793496aa4e0a8916d
Kasper <hagenskasper@gmail.com>
2022-01-04 15:30:36 +0100

    Update Deductionsystem.hs

--- term-rewriting/Deductionsystem.hs
+++ term-rewriting/Deductionsystem.hs
@@ -10,10 +10,10 @@ import Terms (Term(..), Varname, Substitution, appsub)
 import Rules (Basicformula(..), Constraint(..), Rule (..), leftsideR, rightsideR, appsubC, appsubR)
 import Equations
 type Hypothesis = [Rule]
--- Example (from the seminar-document)
+-- Example (from the seminar-document, page 102, slide 30)
 -- sum1 = [R (F "sum1" [V 1]) (F "0" []) (B (V 1 `Le` F "0" [])), R (F "sum1" [V 1]) (F "+" [V 1, F "sum1" [F "-" [V 1, F "1" []]]]) (B (V 1 `Ge` F "0" [])), R (F "+" [V 1, F "return" [V 2]]) (F "return"[F "+" [V 1, V 2]]) (B TT)]
 --
--- sum2 = [R (F "sum2"[V 1]) (F "u" [V 1, F "0" [], F "0" []]) (B TT)]
+-- sum2 = [R (F "sum2"[V 1]) (F "u" [V 1, F "0" [], F "0" []]) (B TT), R (F "u" [V 1, V 2, V 3]) (F "u" [V 1, F "+" [V 2, F "1" [] ], F "+" [V 3, V 2]]) (B (V 2 `Le` V 1)) ]
 type Rules = [Rule]
 type Equations = [Equation]
 type Proofstate = (Equations, Rules)
