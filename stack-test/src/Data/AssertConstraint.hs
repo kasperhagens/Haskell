6afa9b443d272f13245e2d771ded50b760e6e2d0
Kasper <hagenskasper@gmail.com>
2022-01-19 20:30:43 +0100

    a

--- stack-test/src/Data/AssertConstraint.hs
+++ stack-test/src/Data/AssertConstraint.hs
@@ -2,39 +2,15 @@ module Data.AssertConstraint where
 import Z3.Monad
 import Data.Constraints
 import Data.Terms
+import Data.MakeConstraint
 import PrelNames (c1TyConKey)
 
-constraintassertB :: Basicformula -> Z3()
-constraintassertB TT = do
-                    tt <- mkTrue
-                    assert tt
+assertBasicformula :: Basicformula -> Z3()
+assertBasicformula x = do
+                        a <- makeBasicformula x
+                        assert a
 
-constraintassertB FF = do
-                    ff <- mkFalse
-                    assert ff
-
-constraintassertB (Eq t1 t2) = do
-                    a <- makeTerm t1
-                    b <- makeTerm t2
-                    assert =<< (mkEq a b)
-
-constraintassertB (Lt t1 t2) = do
-                    a <- makeTerm t1
-                    b <- makeTerm t2
-                    assert =<< (mkLt a b)
-
-constraintassertB (Le t1 t2) = do
-                    a <- makeTerm t1
-                    b <- makeTerm t2
-                    assert =<< (mkLe a b)
-
-constraintassertB (Gt t1 t2) = do
-                    a <- makeTerm t1
-                    b <- makeTerm t2
-                    assert =<< (mkGt a b)
-
-constraintassertB (Ge t1 t2) = do
-                    a <- makeTerm t1
-                    b <- makeTerm t2
-                    assert =<< (mkGe a b)
-
+assertConstraint :: Constraint -> Z3()
+assertConstraint x = do
+                        a <- makeConstraint x
+                        assert a
--- stack-test/src/Data/MakeConstraint.hs
+++ stack-test/src/Data/MakeConstraint.hs
@@ -2,6 +2,7 @@ module Data.MakeConstraint where
 import Z3.Monad
 import Data.Constraints
 import Data.Terms
+import PrelNames (c1TyConKey)
 
 makeTerm :: Term -> Z3 AST
 makeTerm (V x) = mkFreshIntVar (show (V x))
@@ -24,5 +25,47 @@ makeTerm (F "-" [t1,t2]) = do
 makeTerm t = mkFalse
 
 makeBasicformula :: Basicformula -> Z3 AST
+makeBasicformula TT = mkTrue
+makeBasicformula FF = mkFalse
 
-makeBasicformula TT =
+makeBasicformula (Eq t1 t2) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkEq a b
+
+makeBasicformula (Lt t1 t2) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkLt a b
+
+makeBasicformula (Le t1 t2) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkLe a b
+
+makeBasicformula (Gt t1 t2) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkGt a b
+
+makeBasicformula (Ge t1 t2) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkGe a b
+
+makeConstraint :: Constraint -> Z3 AST
+makeConstraint (B f) = makeBasicformula f
+
+makeConstraint (Or c1 c2) = do
+                                a <- makeConstraint c1
+                                b <- makeConstraint c2
+                                mkOr [a,b]
+
+makeConstraint (And c1 c2) = do
+                                a <- makeConstraint c1
+                                b <- makeConstraint c2
+                                mkAnd [a,b]
+
+makeConstraint (N c) = do
+                                a <- makeConstraint c
+                                mkNot a
