5c7232e67507c0a3d59f8a314f621907107ee7d6
Kasper <hagenskasper@gmail.com>
2022-01-19 15:19:05 +0100

    a

--- stack-test/src/Data/ConstraintAssert.hs
+++ stack-test/src/Data/ConstraintAssert.hs
@@ -1,2 +1,39 @@
 module Data.ConstraintAssert where
 import Z3.Monad
+import Data.Constraints
+import Data.Terms
+
+makeTerm :: Term -> Z3 AST
+makeTerm (V x) = mkFreshIntVar (show (V x))
+makeTerm (F "+" [t1,t2]) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkAdd [a,b]
+
+makeTerm (F "*" [t1,t2]) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkMul [a,b]
+
+makeTerm (F "-" [t1,t2]) = do
+                                a <- makeTerm t1
+                                b <- makeTerm t2
+                                mkSub [a,b]
+
+-- We only allow terms with functionsymbols +, - and *
+makeTerm t= mkFalse
+
+constraintassertB :: Basicformula -> Z3()
+constraintassert TT = do
+                    tt <- mkTrue
+                    assert tt
+
+constraintassertB FF = do
+                    ff <- mkFalse
+                    assert ff
+
+constraintassertB (Eq t1 t2) = do
+                                    a <- makeTerm t1
+                                    b <- makeTerm t2
+                                    assert =<< (mkEq a b)
+
