cde00e96147a1eea8ab6b41fe8b9c160b3f4eea9
Kasper <hagenskasper@gmail.com>
2022-01-24 14:34:31 +0100

    a

--- stack-test/app/Main.hs
+++ stack-test/app/Main.hs
@@ -1,7 +1,44 @@
 module Main where
-
 import Lib
+import Z3.Monad
+import Data.Terms
+import Data.Rules
+import Data.Equations
+import Data.Constraints
+import Data.Zz
+import Data.Char
+import Data.Deductionsystem (
+    Proofstate,
+    Side,
+    showsimps,
+    simplification)
+
+getInteger :: String -> IO Int
+getInteger message =
+    do  putStrLn message -- show a message like "Enter an integer"
+        x <- getLine
+        if all isDigit x
+            then
+                return (read x :: Int)
+            else
+                 do
+                    getInteger "Enter a valid integer"
+
+
+doSimplification :: Int -> Side -> Position -> Rule -> Proofstate -> IO Proofstate
+doSimplification n s p r pfst = do
+    putStrLn "Current proofstate:"
+    print pfst
+    putStrLn "Which equation to simplify? Counting starts at 0."
+    n <- getLine
+    putStrLn "On which side of the equation to simplify: Left or Right?"
+    s <- getLine
+    putStrLn "Enter the position of the subterm to simplify"
+    p <- getLine
+    putStrLn "Which rule to use? Counting starts at 0."
+    r <- getLine
+    return ([],[])
 
 main :: IO ()
 main = do
-    putStrLn "Hello Kasper"
+    print 4
--- stack-test/test/Spec.hs
+++ stack-test/test/Spec.hs
@@ -11,38 +11,9 @@ import Data.Deductionsystem (
     showsimps,
     simplification)
 
-pfst = (eqs, [])
-
-getDigit :: String -> IO Int
-getDigit message =
-    do  putStr message -- like "Enter a number"
-        x <- getChar
-        putStrLn ""
-        if isDigit x
-            then
-                return (digitToInt x)
-            else
-                do
-                    putStrLn ""
-
-
-
-doSimplification :: Int -> Side -> Position -> Rule -> Proofstate -> IO Proofstate
-doSimplification n s p r pfst = do
-    putStrLn "Current proofstate:"
-    print pfst
-    putStrLn "Which equation to simplify? Counting starts at 0."
-    n <- getLine
-    putStrLn "On which side of the equation to simplify: Left or Right?"
-    s <- getLine
-    putStrLn "Enter the position of the subterm to simplify"
-    p <- getLine
-    putStrLn "Which rule to use? Counting starts at 0."
-    r <- getLine
-
 main :: IO ()
 main = do
-
+-- putStrLn (show x )
 --    putStrLn "Test suite not yet implemented"
 --    Z.printResult
 --    printTest
