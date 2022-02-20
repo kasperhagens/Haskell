1b3114194d8619e131413e78d1d53bbd5d8fdfd7
Kasper <hagenskasper@gmail.com>
2022-02-20 01:08:48 +0100

    Update Side.hs

--- stack-test/src/Data/Side.hs
+++ stack-test/src/Data/Side.hs
@@ -3,3 +3,6 @@ import Data.Terms
 
 data Side = Left | Right deriving (Eq, Show, Read)
 
+oppositeSide :: Side -> Side
+oppositeSide Data.Side.Left = Data.Side.Right
+oppositeSide Data.Side.Right = Data.Side.Left
